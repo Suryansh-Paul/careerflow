88152ec2c36230e0f61e9aca228e157accf6aedc
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-07-05 00:52:40 +0530

    Add branded navbar with logo

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,13 @@
 
-
-function Navbar(){
-  return(
-   <nav>careerflow</nav>
-  ) 
-    
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-brand">
+        <img src="/logoecf.jpg" alt="EVANZOFLOW logo" />
+        <span>EVANZOFLOW</span>
+      </div>
+    </nav>
+  );
 }
+
 export default Navbar;
