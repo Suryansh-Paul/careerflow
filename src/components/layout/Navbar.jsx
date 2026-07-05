0ac5051968852204583bc0bee1725aaf61a925c5
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-07-05 16:52:21 +0530

    Add hamburger menu button to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,10 +1,13 @@
-
+import { FiMenu } from "react-icons/fi";
 function Navbar() {
   return (
     <nav>
-      <div className="nav-brand">
+      <div className="nav-left">
+        <button className="menu-btn"><FiMenu/></button>
+        <div className="nav-brand">
         <img src="/logoecf.jpg" alt="EVANZOFLOW logo" />
         <span>EVANZOFLOW</span>
+      </div>
       </div>
     </nav>
   );
