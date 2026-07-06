f32d3256dbad7df93509622bd335bf8dbd0d77f9
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-07-06 15:07:20 +0530

    Add dashboard overview with stat cards

---
+++ src/components/common/Statcard.jsx
@@ -0,0 +1,14 @@
+
+
+function Statcard({title , value}){
+    return (
+        
+        <div className="stat-card">
+            <h1>{title}</h1>
+            <p>{value}</p>
+        </div>
+        
+    )
+}
+
+export default Statcard;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,4 +1,24 @@
+import Statcard from "../components/common/Statcard";
 function DashboardPage(){
-    return <h1>Dashboard</h1>;
+    return (
+        <> 
+            <div id="dashboard">
+                <div className="welcome">
+                 <h1>👋 Welcome back to EVANZOFLOW</h1>
+                   <p>
+                    Track, manage, and organize your entire career journey in one place —
+                    from applications and interviews to offers and resumes.
+                   </p>
+                </div>
+                <div id="stats">
+                    <Statcard title={"Applications"}  value ={"14"}  />
+                    <Statcard title={"Interviews"}  value ={"4"}  />
+                    <Statcard title={"Offers"}  value ={"2"}  />
+                    <Statcard title={"Rejected"}  value ={"10"}  />
+                </div>
+             
+            </div>
+         </>
+    )
 }
 export default DashboardPage;
