9d0a67d7edccc1e09d04fdf29010fbcef05da151
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-08-25 10:21:26 +0530

    feat: add  ErrorResponse DTO

---
+++ backend/src/main/java/com/evan/careerflow/exceptionhandling/ErrorResponse.java
@@ -0,0 +1,32 @@
+package com.evan.careerflow.exceptionhandling;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private List<ValidationError> validationErrors;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ValidationError {
+        private String field;
+        private String message;
+    }
+}
